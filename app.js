a009e3f72e8ba25cee1540ddf1df8c503ed4147d
ahmed elgaml <a7medelgaml11a@gmail.com>
2025-02-12 04:41:13 +0200

    connect to db

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
+// REQUIRING PACKAGES
 const express =require('express');
 const path =require('path');
 const dotenv = require('dotenv');
+dotenv.config({path: path.join(__dirname, './config.env')})
+const mongoose = require('mongoose');
 
 
 
 
 
-
-
-
-
+// CONNECT TO DB
+main()
+.then(() => 
+    console.log('Connected to db' ))
+.catch(err => console.log(err));
+async function main() {
+  await mongoose.connect(process.env.DATABASE);
+}
 
 const app =express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-dotenv.config({path: path.join(__dirname, './config.env')})
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', 'views')
@@ -26,6 +32,11 @@ app.set('views', 'views')
 app.get('/', function(req, res){
     res.send('Welcome');
 })
+
+
+
+
+
 
 
 
